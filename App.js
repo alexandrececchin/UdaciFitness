0d68fe1c493890aa5da0091417d40d353f8b453b
Alexandre Rocha <alexandrececchin@gmail.com>
2019-02-13 21:50:15 -0200

    Add  Itens render components

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
-import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducer';
+import History from './components/History';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={styles.container}>
+        <View style={{ flex: 1 }}>
           <History />
         </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
